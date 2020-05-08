d3c17d95c6c542e6f29198a566fc722fc4cfde0b
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2020-05-08 17:41:32 +0900

    :recycle: if文をswitchで書き直した

--- cpuboard.c
+++ cpuboard.c
@@ -10,6 +10,19 @@
 
 #include <stdio.h>
 
+#define NOP 0x00
+#define HLT 0x0f
+#define LD 0x60
+#define ST 0x70
+
+#define ACC 0x00
+#define IX 0x01
+#define IMMEDIATE_ADDRESS 0x02
+#define ABSOLUTE_PROGRAM_ADDRESS 0x04
+#define ABSOLUTE_DATA_ADDRESS 0x05
+#define IX_MODIFICATION_PROGRAM_ADDRESS 0x06
+#define IX_MODIFICATION_DATA_ADDRESS 0x07
+
 int step_ST(Cpub *cpub);
 int step_LD(Cpub *cpub);
 Uword decrypt_operand_a(const Uword code);
@@ -17,27 +30,11 @@ Uword decrypt_operand_b(const Uword code);
 void unknown_instruction_code(const Uword code);
 void bad_oprand_B(const Uword code);
 
-const Uword ACC = 0x00;
-const Uword IX = 0x01;
-const Uword IMMEDIATE_ADDRESS = 0x02;
-const Uword ABSOLUTE_PROGRAM_ADDRESS = 0x04;
-const Uword ABSOLUTE_DATA_ADDRESS = 0x05;
-const Uword IX_MODIFICATION_PROGRAM_ADDRESS = 0x06;
-const Uword IX_MODIFICATION_DATA_ADDRESS = 0x07;
-
 Uword MAR;
 Uword IR;
 
-/*=============================================================================
- *   Simulation of a Single Instruction
- *===========================================================================*/
 int step(Cpub *cpub) {
     int return_status = RUN_HALT;
-
-    const Uword NOP = 0x00;
-    const Uword HLT = 0x0f;
-    const Uword LD = 0x60;
-    const Uword ST = 0x70;
 
     const Uword MASK = 0xf0;
 
@@ -46,20 +43,32 @@ int step(Cpub *cpub) {
     const Uword INSTRUCTION_CODE = IR & MASK;
     cpub->pc++;
 
-    if ((IR & 0xf8) == NOP) {
-        return RUN_STEP;
-    } else if (((IR & 0xfc) | 0x03) == HLT) {
-        return RUN_HALT;
+    switch (INSTRUCTION_CODE) {
+        case 0x00:
+            /*
+             * 制御命令 + JAL + JR
+             */
+            if ((IR & 0xf8) == NOP) {
+                return_status = RUN_STEP;
+            } else if (((IR & 0xfc) | 0x03) == HLT) {
+                return_status = RUN_HALT;
+            } else {
+                unknown_instruction_code(IR);
+                return_status = RUN_HALT;
+            }
+            break;
+        case LD:
+            return_status = step_LD(cpub);
+            break;
+        case ST:
+            return_status = step_ST(cpub);
+            break;
+        default:
+            unknown_instruction_code(IR);
+            return_status = RUN_HALT;
+            break;
     }
 
-    if (INSTRUCTION_CODE == LD) {
-        return_status = step_LD(cpub);
-    } else if (INSTRUCTION_CODE == ST) {
-        return_status = step_ST(cpub);
-    } else {
-        unknown_instruction_code(IR & MASK);
-        return RUN_HALT;
-    }
     return return_status;
 }
 
@@ -74,22 +83,30 @@ int step_LD(Cpub *cpub) {
     cpub->pc++;
     second_word = cpub->mem[0x000 + MAR];
 
-    if (OPERAND_B == ACC) {
-        operand_b_value = cpub->acc;
-    } else if (OPERAND_B == IX) {
-        operand_b_value = cpub->ix;
-    } else if (OPERAND_B == IMMEDIATE_ADDRESS) {
-        operand_b_value = second_word;
-    } else if (OPERAND_B == ABSOLUTE_PROGRAM_ADDRESS) {
-        operand_b_value = cpub->mem[0x000 + second_word];
-    } else if (OPERAND_B == ABSOLUTE_DATA_ADDRESS) {
-        operand_b_value = cpub->mem[0x100 + second_word];
-    } else if (OPERAND_B == IX_MODIFICATION_PROGRAM_ADDRESS) {
-        operand_b_value = cpub->mem[0x000 + cpub->ix + second_word];
-    } else if (OPERAND_B == IX_MODIFICATION_DATA_ADDRESS) {
-        operand_b_value = cpub->mem[0x100 + cpub->ix + second_word];
-    } else {
-        return RUN_HALT;
+    switch (OPERAND_B) {
+        case ACC:
+            operand_b_value = cpub->acc;
+            break;
+        case IX:
+            operand_b_value = cpub->ix;
+            break;
+        case IMMEDIATE_ADDRESS:
+            operand_b_value = second_word;
+            break;
+        case ABSOLUTE_PROGRAM_ADDRESS:
+            operand_b_value = cpub->mem[0x000 + second_word];
+            break;
+        case ABSOLUTE_DATA_ADDRESS:
+            operand_b_value = cpub->mem[0x100 + second_word];
+            break;
+        case IX_MODIFICATION_PROGRAM_ADDRESS:
+            operand_b_value = cpub->mem[0x000 + cpub->ix + second_word];
+            break;
+        case IX_MODIFICATION_DATA_ADDRESS:
+            operand_b_value = cpub->mem[0x100 + cpub->ix + second_word];
+            break;
+        default:
+            return RUN_HALT;
     }
 
     if (OPERAND_A == ACC) {
@@ -116,35 +133,42 @@ int step_ST(Cpub *cpub) {
         operand_a_value = cpub->ix;
     }
 
-    if (OPERAND_B == ACC) {
-        fprintf(stderr, "ACC is Undefined operating(ST)\n");
-        return RUN_HALT;
-    } else if (OPERAND_B == IX) {
-        fprintf(stderr, "IX is Undefined operating (ST)\n");
-        return RUN_HALT;
-    } else if (OPERAND_B == IMMEDIATE_ADDRESS) {
-        fprintf(stderr, "IMMEDIATE_ADDRESS is Undefined operating in (ST)\n");
-        return RUN_HALT;
-    }
-
     MAR = cpub->pc;
     cpub->pc++;
     second_word = cpub->mem[0x000 + MAR];
 
-    if (OPERAND_B == ABSOLUTE_PROGRAM_ADDRESS) {
-        cpub->mem[0x000 + second_word] = operand_a_value;
-        return_status = RUN_STEP;
-    } else if (OPERAND_B == ABSOLUTE_DATA_ADDRESS) {
-        cpub->mem[0x100 + second_word] = operand_a_value;
-        return_status = RUN_STEP;
-    } else if (OPERAND_B == IX_MODIFICATION_PROGRAM_ADDRESS) {
-        cpub->mem[0x000 + cpub->ix + second_word] = operand_a_value;
-        return_status = RUN_STEP;
-    } else if (OPERAND_B == IX_MODIFICATION_DATA_ADDRESS) {
-        cpub->mem[0x100 + cpub->ix + second_word] = operand_a_value;
-        return_status = RUN_STEP;
-    } else {
-        return RUN_HALT;
+    switch (OPERAND_B) {
+        case ACC:
+            fprintf(stderr, "ACC is Undefined operating(ST)\n");
+            return_status = RUN_HALT;
+            break;
+        case IX:
+            fprintf(stderr, "IX is Undefined operating (ST)\n");
+            return_status = RUN_HALT;
+            break;
+        case IMMEDIATE_ADDRESS:
+            fprintf(stderr,
+                    "IMMEDIATE_ADDRESS is Undefined operating in (ST)\n");
+            return_status = RUN_HALT;
+            break;
+        case ABSOLUTE_PROGRAM_ADDRESS:
+            cpub->mem[0x000 + second_word] = operand_a_value;
+            return_status = RUN_STEP;
+            break;
+        case ABSOLUTE_DATA_ADDRESS:
+            cpub->mem[0x100 + second_word] = operand_a_value;
+            return_status = RUN_STEP;
+            break;
+        case IX_MODIFICATION_PROGRAM_ADDRESS:
+            cpub->mem[0x000 + cpub->ix + second_word] = operand_a_value;
+            return_status = RUN_STEP;
+            break;
+        case IX_MODIFICATION_DATA_ADDRESS:
+            cpub->mem[0x100 + cpub->ix + second_word] = operand_a_value;
+            return_status = RUN_STEP;
+            break;
+        default:
+            return_status = RUN_HALT;
     }
 
     return return_status;
